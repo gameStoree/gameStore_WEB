56c7d31054ac1d475ef84039dae4f4a557c456c4
farhanariyanto03 <ariyantofarhan03@gmail.com>
2024-05-11 17:43:03 +0700

    update apiML
    id dan zone di jadikan satu dalam textfield

--- resources/views/worker/apiMl/index.blade.php
+++ resources/views/worker/apiMl/index.blade.php
@@ -9,14 +9,8 @@
                 <form id="searchForm" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label class="form-label" for="basic-default-company">ID ML</label>
-                        <input type="text" class="form-control" id="basic-default-company" name="id_ml" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="form-label" for="basic-default-email">Server</label>
-                        <div class="input-group input-group-merge">
-                            <input type="text" id="basic-default-email" class="form-control" name="zone" />
-                        </div>
+                        <label class="form-label" for="basic-default-nickname">ID ML (Zone)</label>
+                        <input type="text" class="form-control" id="id_zone" name="nickname" />
                     </div>
                     <button type="submit" class="btn btn-primary">Cari</button>
                 </form>
@@ -27,37 +21,40 @@
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script>
         $(document).ready(function() {
-            $("#searchForm").submit(function(event) {
-                event.preventDefault(); // Menghentikan pengiriman form default
+    $("#searchForm").submit(function(event) {
+        event.preventDefault();
 
-                var idMl = $("#basic-default-company").val();
-                var server = $("#basic-default-email").val();
+        var nickname = $("#id_zone").val();
+        var parts = nickname.split("(");
+        var idMl = parts[0];
+        var zone = parts[1].replace(")", ""); // Hapus tanda kurung tutup
 
-                $.ajax({
-                    url: "https://api.isan.eu.org/nickname/ml",
-                    method: "GET",
-                    data: {
-                        id: idMl,
-                        zone: server
-                    },
-                    success: function(result) {
-                        // Manipulasi data JSON untuk menghilangkan status dan game
-                        var cleanedResult = {
-                            id: result.id,
-                            zoneId: result.zoneId,
-                            name: result.name
-                        };
+        $.ajax({
+            url: "https://api.isan.eu.org/nickname/ml",
+            method: "GET",
+            data: {
+                id: idMl,
+                zone: zone
+            },
+            success: function(result) {
+                // Manipulasi data JSON untuk menghilangkan status dan game
+                var cleanedResult = {
+                    id: result.id,
+                    zoneId: result.zoneId,
+                    name: result.name
+                };
 
-                        // Tampilkan hasil yang telah dibersihkan
-                        $("#result").html(JSON.stringify(cleanedResult));
-                    },
-                    error: function(xhr, status, error) {
-                        // Handle error
-                        console.log(xhr.responseText);
-                    }
-                });
-            });
+                // Tampilkan hasil yang telah dibersihkan
+                $("#result").html(JSON.stringify(cleanedResult));
+            },
+            error: function(xhr, status, error) {
+                // Handle error
+                console.log(xhr.responseText);
+            }
         });
+    });
+});
+
     </script>
 
     @include('sweetalert::alert')
